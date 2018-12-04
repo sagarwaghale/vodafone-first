25a695329da24780936cf78c23039a98d96dd483
sagarwaghale <sagarwaghale2@gmail.com>
2018-12-04 19:05:54 +0530

    added js

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,4 +1,52 @@
-var app = angular.module('myapp',['pascalprecht.translate']);
+var app = angular.module('myapp',[]);
+
+app.controller('myCtrl',['$scope','$http' , function ($scope,$http) {
+
+/*        $scope.ArtistsController = ArtistsController;
+    $scope.makePayment = makePayment;
+
+    function ArtistsController($scope, $http) {
+        console.log("samfkldmklgmklfmh");
+        $http.get('property/summary.json').success(function (data) {
+            $scope.data = data;
+        });
+    }
+
+    function makePayment() {
+        console.log('makePayment');
+    }
+
+  */
+    var totalAmount = 0;
+    var newbill = 0;
+
+$scope.ArtistsController = function () {
+    console.log("samfkldmklgmklfmh");
+    $http.get('property/summary.json').success(function (data) {
+        $scope.data = data;
+        for (var x = 0; x < data.length; x++) {
+            totalAmount = data[x].balance;
+        }
+
+    });
+}
+
+$scope.makePayment = function () {
+   var bill = $scope.bill ;
+    newbill = totalAmount - bill;
+    console.log(newbill);
+
+}
+}]);
+
+/*app.controller('myCtrl', function ArtistsController($scope, $http) {
+    console.log("samfkldmklgmklfmh");
+    $http.get('property/summary.json').success(function (data) {
+        $scope.artists = data;
+        console.log($scope.artists);
+    });
+});*/
+
 
 /*
 app.config(['translateProvider', function ($translateProvider) {
@@ -31,10 +79,4 @@ app.config(['translateProvider', function ($translateProvider) {
     };
 }]);*/
 
-app.controller('myCtrl', function ArtistsController($scope, $http) {
-    console.log("samfkldmklgmklfmh");
-    $http.get('property/summary.json').success(function (data) {
-        $scope.artists = data;
-        console.log($scope.artists);
-    });
-});
+
